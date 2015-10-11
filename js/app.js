6eebc9431c99a4ac84f0855177f6720d2fc56bd7
libby-barker <esb1244@gmail.com>
2015-10-11 00:09:06 -0400

    added hadouken sound

--- js/app.js
+++ js/app.js
@@ -4,24 +4,30 @@ $(document).ready(function() {
 		$('.ryu-ready').show();
 	})
 	.mouseleave(function() {
-		$('.ryu-ready').hide();
 		$('.ryu-still').show();
+		$('.ryu-ready').hide();
 	})
-	.mousedown(function() {
+	.mousedown(function(){
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').finish().show()
-			.animate(
-				{'left': '1020px'},
-				500,
-				function() {
-					$(this).hide();
-					$(this).css('left', '520px');
-				}
-			);
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+			});
 	})
-	.mouseup(function() {
+	.mouseup(function(){
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
-}); 
+
+});
+
+	function playHadouken () {
+		$('#hadouken-sound')[0].volume = 0.5;
+		$('#hadouken-sound')[0].load();
+		$('#hadouken-sound')[0].play();
+	}
